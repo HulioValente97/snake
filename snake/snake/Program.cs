93ff7fbc3345c2116a84bf6ac42dc970c5e1b605
HulioValente97 <d_buken@mail.ru>
2017-07-31 19:13:12 +0600

    Создан Class Snake

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -22,9 +22,10 @@ namespace snake
             reightline.Drow();
 
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Point p = new Point(4, 5, '*');
 
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
             Console.ReadLine();
         }
     }
